7dbdb93e9682dc89a296066934a04a937c7dd701
randr97 <rohitshrothrium.ss1997@gmail.com>
2019-03-20 13:35:21 +0530

    Modified settings.py to ALLOW_HOST

--- application/backend_entry/backend/dams_api/settings.py
+++ application/backend_entry/backend/dams_api/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default="True")
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
 # Application definition
 
 INSTALLED_APPS = [
