3048fd2092ab1c774dd03f3afd9211d93f4e7186
randr97 <rohitshrothrium.ss1997@gmail.com>
2019-03-20 13:31:37 +0530

    changes made on docker files

--- application/backend/assetmanagement/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class assetmanagementConfig(AppConfig):
-    name = 'assetmanagement'
--- application/react-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /application/frontend
-echo "Checking for new NPM Packages"
-npm install
-echo "Starting React App Server"
-npm start
